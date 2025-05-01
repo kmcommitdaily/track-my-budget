f3449f20f8ab942f676256d2b07e068765e669d1
kimberly <kmgalos0909@gmail.com>
2025-05-01 08:38:29 +0800

    fix: edit baseURL vercel

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from 'better-auth/react';
 
 export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000', // Your app's base URL
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL!,
+  // Your app's base URL
 });
 
 // Export auth functions for easy use
