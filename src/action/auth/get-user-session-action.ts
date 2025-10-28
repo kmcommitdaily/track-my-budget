f9591340363290a662fde296da206ca8470ae2e6
Kimberly Galos <kimberlygalos@Kimberlys-MacBook-Air.local>
2025-10-28 12:04:33 +0800

    refactor: make get user session reusable

---
+++ src/action/auth/get-user-session-action.ts
@@ -0,0 +1,11 @@
+"use server";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+export async function getSessionAction() {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+  return session;
+}
